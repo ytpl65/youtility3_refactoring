e7059901e2b919b08fea1fe1e29bcdd7034f28a4
naveen <naveen@1d779faf-a863-44aa-9b90-f08c3dab5e60>
2021-07-10 08:00:40 +0000

    First Copy

---
+++ intelliwiz_config/jinja/env.py
@@ -0,0 +1,15 @@
+from jinja2.environment import Environment
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import reverse
+
+
+def debug(info):
+    print("Printing=============", info)
+
+class JinjaEnvironment(Environment):
+    def __init__(self, **kwargs):
+        super(JinjaEnvironment, self).__init__(**kwargs)
+        self.globals['static']  = staticfiles_storage.url
+        self.globals['url'] = reverse
+        self.filters["debug"] = debug
+    
