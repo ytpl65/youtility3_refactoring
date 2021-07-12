9bd1f889f3174f1c3d191c7fc8e4b2c5293ee6d4
naveen <naveen@1d779faf-a863-44aa-9b90-f08c3dab5e60>
2021-07-12 07:15:04 +0000

    registered models (Bt, TypeAssist) in admin site.

--- apps/onboarding/admin.py
+++ apps/onboarding/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import TypeAssist, Bt
+# Register your models here.
+
+@admin.register(TypeAssist)
+class TypeAssistAdmin(admin.ModelAdmin):
+    fields = ('tacode', 'taname', 'tatype', 'parent', 'buid',)
+    list_display = ('tacode', 'taname', 'tatype', 'parent',)
+    list_display_links = ('tacode',)
+    
+
+@admin.register(Bt)
+class BtAdmin(admin.ModelAdmin):
+    fields = ('btid','bucode', 'buname', 'butype', 'parent', 'gpslocation',
+                'iswarehouse', 'enable', 'bu_preferences')
+    exclude = ['bupath']
+    list_display = ('bucode', 'buname', 'butype', 'parent', 'bupath')
+    list_display_links = ('bucode',)
