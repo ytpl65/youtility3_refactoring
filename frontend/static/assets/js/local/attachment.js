a1f5e81b1dc6708d212e20063b6bd3bbde3e66d1
Naveen Sargam <naveen.sargam@youtility.in>
2022-08-30 06:30:29 +0530

    style issues fixed

--- frontend/static/assets/js/local/attachment.js
+++ frontend/static/assets/js/local/attachment.js
@@ -1,89 +1,82 @@
 var attTable;
 $(document).ready(() => {
-    attTable =  $("#tabAttachment").DataTable({
-        deferRender:true,
-        responsive:true,
-        dom:'lrtp',
-        columns:[
-            {data:'id', targets:0},
-            {data:'filepath', targets:1},
-            {data:'filename', targets:2},
-            {data:'filename', targets:2},
-        ]
+  attTable = $("#tabAttachment").DataTable({
+    deferRender: true,
+    responsive: true,
+    dom: "lrtp",
+    columns: [
+      { data: "id", targets: 0 },
+      { data: "filepath", targets: 1 },
+      { data: "filename", targets: 2 },
+      { data: "filename", targets: 2 },
+    ],
+  });
+  $("#id_attachment").click(() => {
+    $("#popup_attachment").modal("show");
+  });
+  $("#popup_attachment").on("shown.bs.modal", () => {
+    //get attachement data from ajax get
+    var data = getAttachmentData(attachmentOwner);
 
-    })
-    $("#id_attachment").click(() => {
-        $("#popup_attachment").modal('show')
-    })
-     $("#popup_attachment").on('shown.bs.modal', () => {
-         //get attachement data from ajax get
-        var data = getAttachmentData(attachmentOwner)
-        
-        if(data){
-            attTable.clear()
-            attTable.rows.add(data).draw()
-        }
-     })
+    if (data) {
+      attTable.clear();
+      attTable.rows.add(data).draw();
+    }
+  });
 
-     $("#btnuploadattachment").click(function(){
-        var uploadfile = $("#attachmentfile").val();
-        if(uploadfile.length == 0){
-            $("#nonfield_errors span").html('Please select a file to upload.')
-            $("#nonfield_errors").show()
-        }
-        else{
-            formdata= new FormData();
-            formdata.append("oper", "add");
-            formdata.append("id", "");
-            formdata.append("csrfmiddlewaretoken", csrf);
-            formdata.append("img", $('#attachmentfile')[0].files[0]);
-            formdata.append("dataSource", "attachment");
-            formdata.append("foldertype", folderType);
-            formdata.append("isDefault", isDefault);
-            formdata.append("ownerid", attachmentOwner);
-            formdata.append("attachmenttype", "ATTACHMENT");
-            formdata.append("ownername", ownername);
-            formdata.append("docnumber", docnumber);
-            formdata.append("ctzoffset", $("#id_ctzoffset").val());
-            formdata.append("doctype", 'None');
-            if (formdata) {
-                $.ajax({
-                    url: attachmentUrl,
-                    type: "POST",
-                    data: formdata,
-                    processData: false,
-                    contentType: false,
-                    //csrfmiddlewaretoken: '{{ csrf_token }}',
-                    success: function(data) {
-                        if(data.rc==1){
-                            $("#nonfield_errors span").html('Upload Failed')
-                            $("#nonfield_errors").show()
-                        }else{
-
-                        }
-                    }
-                });
+  $("#btnuploadattachment").click(function () {
+    var uploadfile = $("#attachmentfile").val();
+    if (uploadfile.length === 0) {
+      $("#nonfield_errors span").html("Please select a file to upload.");
+      $("#nonfield_errors").show();
+    } else {
+      formdata = new FormData();
+      formdata.append("oper", "add");
+      formdata.append("id", "");
+      formdata.append("csrfmiddlewaretoken", csrf);
+      formdata.append("img", $("#attachmentfile")[0].files[0]);
+      formdata.append("dataSource", "attachment");
+      formdata.append("foldertype", folderType);
+      formdata.append("isDefault", isDefault);
+      formdata.append("ownerid", attachmentOwner);
+      formdata.append("attachmenttype", "ATTACHMENT");
+      formdata.append("ownername", ownername);
+      formdata.append("docnumber", docnumber);
+      formdata.append("ctzoffset", $("#id_ctzoffset").val());
+      formdata.append("doctype", "None");
+      if (formdata) {
+        $.ajax({
+          url: attachmentUrl,
+          type: "POST",
+          data: formdata,
+          processData: false,
+          contentType: false,
+          //csrfmiddlewaretoken: '{{ csrf_token }}',
+          success: function (data) {
+            if (data.rc === 1) {
+              $("#nonfield_errors span").html("Upload Failed");
+              $("#nonfield_errors").show();
+            } else {
             }
-        }
-    })
+          },
+        });
+      }
+    }
+  });
 
-    $("#clearattachment").click(function(){
-        $('input[type=file]').val('');
-    });
-})
+  $("#clearattachment").click(function () {
+    $("input[type=file]").val("");
+  });
+});
 
-
-function getAttachmentData(uuid){
-    fire_ajax_get({
-        url:attachmentUrl,
-        data:{'owner':uuid}
-    }).done((data, status, xhr) => {
-        if(data){
-            
-            attTable.clear()
-            attTable.rows.add(data).draw()
-        }
-    })
+function getAttachmentData(uuid) {
+  fire_ajax_get({
+    url: attachmentUrl,
+    data: { owner: uuid },
+  }).done((data, status, xhr) => {
+    if (data) {
+      attTable.clear();
+      attTable.rows.add(data).draw();
+    }
+  });
 }
-
-
--- frontend/static/assets/js/local/wizard.js
+++ frontend/static/assets/js/local/wizard.js
@@ -1,108 +1,92 @@
+if (Object.prototype.hasOwnProperty.call(session, "wizard_data")) {
+  //Alert config for quit wizard
+  const quitwizard_config = {
+    title: "Quit Wizard",
+    text: "Do you want to save changes.",
+    icon: "warning",
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: "Save As Draft",
+    denyButtonText: "Don't Save",
+  };
 
-if (Object.prototype.hasOwnProperty.call(session, 'wizard_data')) {
-    //Alert config for quit wizard
-    const quitwizard_config = {
-      title: "Quit Wizard",
-      text: "Do you want to save changes.",
-      icon: "warning",
-      showDenyButton: true,
-      showCancelButton: true,
-      confirmButtonText: "Save As Draft",
-      denyButtonText: "Don't Save",
-    };
-  
-    //Alert config for quit wizard
-    const openWizardConfig = {
-      title: "Onboarding Wizard",
-      text: "Do you want to continue the wizard saved in draft or start a new wizard",
-      icon: "info",
-      showDenyButton: true,
-      showCancelButton: true,
-      confirmButtonText: "Continue the wizard",
-      denyButtonText: "Start a new wizard",
-    };
-  
-    
-  
-    $(document).ready(() => {
-        if (checkWizardInDraft) {
-          /*display options to load from 
+  //Alert config for quit wizard
+  const openWizardConfig = {
+    title: "Onboarding Wizard",
+    text: "Do you want to continue the wizard saved in draft or start a new wizard",
+    icon: "info",
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: "Continue the wizard",
+    denyButtonText: "Start a new wizard",
+  };
+
+  $(document).ready(() => {
+    if (checkWizardInDraft) {
+      /*display options to load from 
           draft or start a new wizard*/
-          Swal.fire(openWizardConfig).then((result) => {
-            if (result.isConfirmed) {
-              
-              //open draft wizard
-              populateWizard();
-            } else if (result.isDenied) {
-              
-              //open new wizard from start
-              window.location.href = newWizardUrl;
-              
-            }
-          });
+      Swal.fire(openWizardConfig).then((result) => {
+        if (result.isConfirmed) {
+          //open draft wizard
+          populateWizard();
+        } else if (result.isDenied) {
+          //open new wizard from start
+          window.location.href = newWizardUrl;
         }
-      $("#id_quitwizard").click(() => {
-        
-        Swal.fire(quitwizard_config).then((result) => {
-          if (result.isConfirmed) {
-            
-            //save as draft execution
-            saveAsDraft();
-          } else if (result.isDenied) {
-            
-            window.location.href = deleteWizardUrl;
-          }
-        });
+      });
+    }
+    $("#id_quitwizard").click(() => {
+      Swal.fire(quitwizard_config).then((result) => {
+        if (result.isConfirmed) {
+          //save as draft execution
+          saveAsDraft();
+        } else if (result.isDenied) {
+          window.location.href = deleteWizardUrl;
+        }
       });
     });
-  
-    let formId = session["wizard_data"]["formid"];
-    const current_location = currentUrl;
-    const formIdentifier = `${current_location} ${formId}`;
-    const wizard_data = session["wizard_data"];
-    let form = $(`#${formId}`);
-  
-    const getFormData = () => {
-      
-      let data = { wizard_data: wizard_data };
-      return data;
-    };
-  
-    const saveAsDraft = () => {
-      
-      data = getFormData();
-      
-      localStorage.setItem("formData", JSON.stringify(data));
-    };
-  
-    let form_url = "";
-  
-    const populateWizard = () => {
-      
-      if (localStorage.key("formData")) {
-        
-        const savedData = JSON.parse(localStorage.getItem("formData"));
-        
-        if (setSessionInRequest(savedData) == true) {
-          
-          const message = "wizard has been continued from last saved location";
-          Swal.fire({
-            title: "Wizard Loaded",
-            text: message,
-            confirmButtonText: "Okay Got It!",
-          });
-          //remove key after form loaded
-          localStorage.removeItem("formData");
-          
-          loadWizard(savedData["wizard_data"], true);
-        }
-        window.location.href = newWizardUrl;
+  });
+
+  let formId = session["wizard_data"]["formid"];
+  const current_location = currentUrl;
+  const formIdentifier = `${current_location} ${formId}`;
+  const wizard_data = session["wizard_data"];
+  let form = $(`#${formId}`);
+
+  const getFormData = () => {
+    let data = { wizard_data: wizard_data };
+    return data;
+  };
+
+  const saveAsDraft = () => {
+    data = getFormData();
+
+    localStorage.setItem("formData", JSON.stringify(data));
+  };
+
+  let form_url = "";
+
+  const populateWizard = () => {
+    if (localStorage.key("formData")) {
+      const savedData = JSON.parse(localStorage.getItem("formData"));
+
+      if (setSessionInRequest(savedData) === true) {
+        const message = "wizard has been continued from last saved location";
+        Swal.fire({
+          title: "Wizard Loaded",
+          text: message,
+          confirmButtonText: "Okay Got It!",
+        });
+        //remove key after form loaded
+        localStorage.removeItem("formData");
+
+        loadWizard(savedData["wizard_data"], true);
       }
-    };
-  
-    $("#save_wizard_form").click(() => {
-      
-      saveAsDraft();
-      
-    });
-  }
+      window.location.href = newWizardUrl;
+    }
+  };
+
+  $("#save_wizard_form").click(() => {
+    saveAsDraft();
+  });
+}
