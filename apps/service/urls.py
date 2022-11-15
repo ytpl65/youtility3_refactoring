f940b8ca654d38c90a5cc09239b9a2f0fea75256
redmine <repo.ytpl@gmail.com>
2022-11-15 19:00:29 +0530

    urls remove service

--- apps/service/urls.py
+++ apps/service/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
-from apps.service import views
+# from django.urls import path
+# from apps.service import views
 
-urlpatterns = [
-    path('insert-record/', views.InsertRecord.as_view(), name='insert_record'),
-    path('tasktourupdate/', views.TaskTourUpdate.as_view(), name='tasktourupdate'),
-    path('template-report/', views.TemplateReports.as_view(), name='template_report'),
-    path('upload-attachment/', views.AttachmentUpload.as_view(), name='upload_attachment'),
-]
+# urlpatterns = [
+#     path('insert-record/', views.InsertRecord.as_view(), name='insert_record'),
+#     path('tasktourupdate/', views.TaskTourUpdate.as_view(), name='tasktourupdate'),
+#     path('template-report/', views.TemplateReports.as_view(), name='template_report'),
+#     path('upload-attachment/', views.AttachmentUpload.as_view(), name='upload_attachment'),
+# ]
