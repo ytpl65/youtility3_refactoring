aef88762e08c40346b967a8a8d757c6692b8afba
redmine <repo.ytpl@gmail.com>
2022-12-12 12:01:01 +0530

    sync mutation logging fixed

--- apps/service/mutations.py
+++ apps/service/mutations.py
@@ -240,7 +240,7 @@ class SyncMutation(graphene.Mutation):
         from apps.service import tasks
         try:
             db = get_current_db_name()
-            log.info('the type of file', type(file))
+            log.info(f'the type of file is {type(file)}')
             with zipfile.ZipFile(file) as zip:
                 zipsize = TR = 0
                 for file in zip.filelist:
