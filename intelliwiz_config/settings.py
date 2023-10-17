bbd49751fb4a44dd8f65e223837dc4bf5ef65e21
manohar <manohar.lagishetty@youtility.in>
2023-10-17 13:41:02 +0530

    Written work permit form submission without values issue resolved.

--- intelliwiz_config/settings.py
+++ intelliwiz_config/settings.py
@@ -43,7 +43,7 @@ ENCRYPT_KEY = env('ENCRYPT_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True    
 
-ALLOWED_HOSTS = ['.localhost', 'demo.youtility.in', 'redmine.youtility.in']
+ALLOWED_HOSTS = ['.localhost', 'demo.youtility.in', 'redmine.youtility.in', '127.0.0.1' ]
 
 # Application definition
 
