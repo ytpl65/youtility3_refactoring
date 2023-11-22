3710de6eb48d6ce45e4b9cd97640edd63be7694d
manohar <manohar.lagishetty@youtility.in>
2023-11-22 15:58:05 +0530

    working on list of tasks report

--- apps/reports/utils.py
+++ apps/reports/utils.py
@@ -183,11 +183,13 @@ class ReportEssentials(object):
         from apps.reports.report_designs.task_summary import TaskSummaryReport
         from apps.reports.report_designs.tour_summary import TourSummaryReport
         from apps.reports.report_designs.ppm_summary import PPMSummaryReport
+        from apps.reports.report_designs.list_of_task import ListofTaskReport
         
         return {
             self.TaskSummary: TaskSummaryReport,
             self.TourSummary:TourSummaryReport,
-            self.PPMSummary:PPMSummaryReport
+            self.PPMSummary:PPMSummaryReport,
+            self.ListOfTasks:ListofTaskReport
         }.get(self.report_name)
     
         
